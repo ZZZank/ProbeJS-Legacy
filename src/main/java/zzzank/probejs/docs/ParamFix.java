183c870ef273ae4dbe6c78440cbb9570d3f875fb
ZZZank <3410764033@qq.com>
2025-03-04 13:30:52 +0800

    fix param of `recipeEvent.custom(...)`

--- src/main/java/zzzank/probejs/docs/ParamFix.java
+++ src/main/java/zzzank/probejs/docs/ParamFix.java
@@ -43,7 +43,7 @@ public class ParamFix implements ProbeJSPlugin {
                             )
                         )
                     )
-                    .literalMember("[x in string]", Types.ANY)
+                    .literalMember("[x: string]", Types.ANY)
                     .build()
                     .comment("other recipe json elements are unknown to ProbeJS :(")
             );
