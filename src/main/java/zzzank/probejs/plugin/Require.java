3e2c814628eaf35b9f55ff3190a897500d6fc558
ZZZank <3410764033@qq.com>
2025-03-29 14:28:07 +0800

    warn users when using `require` with `isolatedScopes` disabled

--- src/main/java/zzzank/probejs/plugin/Require.java
+++ src/main/java/zzzank/probejs/plugin/Require.java
@@ -4,6 +4,7 @@ import dev.latvian.kubejs.script.ScriptManager;
 import dev.latvian.mods.rhino.*;
 import dev.latvian.mods.rhino.util.HideFromJS;
 import lombok.val;
+import zzzank.probejs.ProbeConfig;
 import zzzank.probejs.lang.java.clazz.ClassPath;
 
 public class Require extends BaseFunction {
@@ -15,6 +16,12 @@ public class Require extends BaseFunction {
 
     @Override
     public Object call(Context cx, Scriptable scope, Scriptable self, Object[] args) {
+        if (!ProbeConfig.isolatedScopes.get()) {
+            manager.type.console.warn(String.format(
+                "'require(...)' used without enabling '%s' config, this might cause inconsistency between IDE report and actual behaviour",
+                ProbeConfig.isolatedScopes.name
+            ));
+        }
         String name;
         if (args.length == 0 || !(name = args[0].toString()).startsWith(ClassPath.TS_PATH_PREFIX)) {
             return new RequireWrapper(null, Undefined.instance);
@@ -24,7 +31,10 @@ public class Require extends BaseFunction {
         try {
             return new RequireWrapper(path, manager.loadJavaClass(scope, new String[]{name}));
         } catch (Exception ignored) {
-            manager.type.console.warn(String.format("Class '%s' not loaded", path.getJavaStylePath()));
+            manager.type.console.error(String.format(
+                "Class '%s' not found, returning undefined value",
+                path.getJavaStylePath()
+            ));
             return new RequireWrapper(path, Undefined.instance);
         }
     }
