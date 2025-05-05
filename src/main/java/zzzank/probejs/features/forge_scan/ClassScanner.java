e630ac6f8db75b520fb77dc96e76f190555acad1
ZZZank <3410764033@qq.com>
2025-05-05 19:20:34 +0800

    safer mod jar scanning

--- src/main/java/zzzank/probejs/features/forge_scan/ClassScanner.java
+++ src/main/java/zzzank/probejs/features/forge_scan/ClassScanner.java
@@ -1,14 +1,13 @@
 package zzzank.probejs.features.forge_scan;
 
 import lombok.val;
-import me.shedaniel.architectury.platform.Mod;
 import me.shedaniel.architectury.platform.Platform;
 import net.minecraftforge.fml.ModList;
 import org.jetbrains.annotations.NotNull;
 import zzzank.probejs.ProbeConfig;
+import zzzank.probejs.ProbeJS;
 import zzzank.probejs.utils.ReflectUtils;
 
-import java.nio.file.Path;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
@@ -36,8 +35,15 @@ public class ClassScanner {
     public static @NotNull List<Class<?>> scanMods(Collection<String> modids) {
         return modids.stream()
             .map(Platform::getMod)
-            .map(Mod::getFilePath)
-            .map(Path::toFile)
+            .map(mod -> {
+                try {
+                    return mod.getFilePath().toFile();
+                } catch (Exception e) {
+                    ProbeJS.LOGGER.error("unable to locate file for mod '{}'", mod.getModId(), e);
+                    return null;
+                }
+            })
+            .filter(Objects::nonNull)
             .map(ModJarClassScanner::scanFile)
             .flatMap(Collection::stream)
             .collect(Collectors.toList());
