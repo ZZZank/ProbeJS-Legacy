9817363a905ea50505f37634b4bc4c827250a5a6
ZZZank <3410764033@qq.com>
2025-05-24 18:13:44 +0800

    fix no default generic param for types read from KubeJS

--- src/main/java/zzzank/probejs/features/kesseractjs/TypeDescAdapter.java
+++ src/main/java/zzzank/probejs/features/kesseractjs/TypeDescAdapter.java
@@ -2,14 +2,14 @@ package zzzank.probejs.features.kesseractjs;
 
 import dev.latvian.mods.kubejs.typings.desc.*;
 import lombok.val;
-import zzzank.probejs.lang.java.clazz.ClassPath;
+import zzzank.probejs.lang.java.remap.RemapperBridge;
 import zzzank.probejs.lang.typescript.code.type.BaseType;
 import zzzank.probejs.lang.typescript.code.type.Types;
 import zzzank.probejs.lang.typescript.code.type.js.JSJoinedType;
-import zzzank.probejs.lang.typescript.code.type.ts.TSClassType;
 import zzzank.probejs.lang.typescript.code.type.ts.TSParamType;
 
 import java.util.Arrays;
+import java.util.List;
 
 /**
  * @author ZZZank
@@ -23,48 +23,51 @@ public class TypeDescAdapter {
         }
     };
 
+    public static List<BaseType> convertTypes(TypeDescJS... descJSs) {
+        return switch (descJSs.length) {
+            case 0 -> List.of();
+            case 1 -> List.of(convertType(descJSs[0]));
+            case 2 -> List.of(convertType(descJSs[0]), convertType(descJSs[1]));
+            default -> Arrays.stream(descJSs).map(TypeDescAdapter::convertType).toList();
+        };
+    }
+
     public static BaseType convertType(TypeDescJS typeDesc) {
-        if (typeDesc instanceof ArrayDescJS arrayDesc) {
-            return convertType(arrayDesc.type()).asArray();
-        } else if (typeDesc instanceof FixedArrayDescJS fixedArrayDesc) {
-            return Types.join(",", "[", "]", Arrays.stream(fixedArrayDesc.types())
-                .map(TypeDescAdapter::convertType)
-                .toList());
-        } else if (typeDesc instanceof GenericDescJS genericDesc) {
-            if (genericDesc.type() instanceof PrimitiveDescJS primitiveDesc && primitiveDesc.type().equals("Map")) {
-                if (genericDesc.types().length != 2) {
+        if (typeDesc instanceof ArrayDescJS(TypeDescJS component)) {
+            return convertType(component).asArray();
+        } else if (typeDesc instanceof FixedArrayDescJS(TypeDescJS[] types)) {
+            return Types.join(",", "[", "]", convertTypes(types));
+        } else if (typeDesc instanceof GenericDescJS(TypeDescJS base, TypeDescJS[] params)) {
+            if (base instanceof PrimitiveDescJS(String value) && value.equals("Map")) {
+                if (params.length != 2) {
                     return Types.ANY;
                 }
-                val valueType = convertType(genericDesc.types()[1]);
+                val valueType = convertType(params[1]);
                 return Types.custom(
                     (decl, formatType) -> "{[k: string]: %s}".formatted(valueType.line(decl, formatType)),
                     valueType::getImportInfos
                 );
             }
 
-            return new TSParamType(
-                convertType(genericDesc.type()),
-                Arrays.stream(genericDesc.types()).map(TypeDescAdapter::convertType).toList()
-            );
-        } else if (typeDesc instanceof ObjectDescJS objectDesc) {
+            return new TSParamType(convertType(base), convertTypes(params));
+        } else if (typeDesc instanceof ObjectDescJS(java.util.List<ObjectDescJS.Entry> types)) {
             val builder = Types.object();
-            for (val type : objectDesc.types()) {
+            for (val type : types) {
                 builder.member(type.key(), type.optional(), convertType(type.value()));
             }
             return builder.build();
-        } else if (typeDesc instanceof OrDescJS orDesc) {
-            return new JSJoinedType.Union(
-                Arrays.stream(orDesc.types()).map(TypeDescAdapter::convertType).toList());
-        } else if (typeDesc instanceof PrimitiveDescJS primitiveDesc) {
-            String content = primitiveDesc.type();
-            if (content.startsWith(PROBEJS_PREFIX)) {
-                content = content.substring(PROBEJS_PREFIX.length());
-                val parts = content.split("\\.");
-                parts[parts.length - 1] = "$" + parts[parts.length - 1];
-                return new TSClassType(new ClassPath(Arrays.stream(parts).toArray(String[]::new)));
-            } else {
-                return Types.primitive(content);
+        } else if (typeDesc instanceof OrDescJS(TypeDescJS[] types)) {
+            return new JSJoinedType.Union(convertTypes(types));
+        } else if (typeDesc instanceof PrimitiveDescJS(String value)) {
+            if (value.startsWith(PROBEJS_PREFIX)) {
+                val className = RemapperBridge.unmapClass(value.substring(PROBEJS_PREFIX.length()));
+                try {
+                    return Types.typeMaybeGeneric(Class.forName(className));
+                } catch (Throwable e) {
+                    throw new RuntimeException(e);
+                }
             }
+            return Types.primitive(value);
         }
 
         throw new RuntimeException("Unknown TypeDescJS");
