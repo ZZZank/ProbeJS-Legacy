5d331f32089305ebf235b34bcc474b1843b3940f
ZZZank <3410764033@qq.com>
2025-06-09 21:33:39 +0800

    setup JUnit
    (cherry picked from commit 135e035f35f6f75d3e936aa9450be635bc4df718)

---
+++ src/test/java/org/example/ExampleTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author ZZZank
+ */
+public class ExampleTest {
+
+    @Test
+    public void test() {
+        Assertions.assertEquals(1, 1);
+    }
+}
