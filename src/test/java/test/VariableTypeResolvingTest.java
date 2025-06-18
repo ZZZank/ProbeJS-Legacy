c33cee5150e77ba33b8ea4cc56fe2f079e3479d5
ZZZank <3410764033@qq.com>
2025-06-18 20:27:34 +0800

    fix variable type resolving

---
+++ src/test/java/test/VariableTypeResolvingTest.java
@@ -0,0 +1,39 @@
+package test;
+
+import com.google.common.collect.ImmutableList;
+import it.unimi.dsi.fastutil.longs.LongComparator;
+import lombok.val;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.common.world.ForgeChunkManager;
+import net.minecraftforge.fml.RegistryObject;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import zzzank.probejs.lang.java.ClassRegistry;
+
+import java.util.Comparator;
+import java.util.Map;
+
+/**
+ * @author ZZZank
+ */
+public class VariableTypeResolvingTest {
+
+    @ParameterizedTest
+    @ValueSource(classes = {
+        LongComparator.class,
+        Map.Entry.class,
+        Comparator.class,
+        ImmutableList.class,
+        RegistryObject.class,
+        ForgeConfigSpec.Builder.class,
+        ForgeChunkManager.TicketTracker.class
+    })
+    public void test(Class<?> type) {
+        val classReg = new ClassRegistry();
+
+        val clazz = classReg.addClass(type);
+        // stack overflow will happen before
+        Assertions.assertNotNull(clazz);
+    }
+}
