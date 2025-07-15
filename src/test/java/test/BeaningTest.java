63e67f34b993e6de0ccd5ca6801137c20f73a7b8
ZZZank <3410764033@qq.com>
2025-07-15 21:04:14 +0800

    add a test for beaning

---
+++ src/test/java/test/BeaningTest.java
@@ -0,0 +1,78 @@
+package test;
+
+import lombok.val;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import zzzank.probejs.lang.java.ClassRegistry;
+import zzzank.probejs.lang.java.clazz.ClassPath;
+import zzzank.probejs.lang.transpiler.Transpiler;
+import zzzank.probejs.lang.typescript.code.member.BeanDecl;
+import zzzank.probejs.lang.typescript.code.member.ClassDecl;
+import zzzank.probejs.lang.typescript.code.type.js.JSPrimitiveType;
+
+/**
+ * @author ZZZank
+ */
+public class BeaningTest {
+
+    static {
+        InitFMLPathsTest.init();
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {Interf.class, AbsCls.class})
+    public void test(Class<?> type) {
+        val classRegistry = new ClassRegistry();
+        classRegistry.addClass(type);
+        classRegistry.walkClass();
+
+        val transpiler = new Transpiler();
+        transpiler.init();
+
+        val files = transpiler.dump(classRegistry.getFoundClasses());
+        val classDecl = files.get(ClassPath.fromJava(type))
+            .findCode(ClassDecl.class)
+            .orElseThrow();
+
+        var hasMatchingBean = false;
+        for (var code : classDecl.bodyCode) {
+            if (code instanceof BeanDecl beanDecl) {
+                switch (beanDecl.name) {
+                    case "" -> throw new AssertionError("empty");
+                    case "Str" -> throw new AssertionError("Str");
+                    case "str" -> {
+                        hasMatchingBean = true;
+                        Assertions.assertEquals(
+                            "string",
+                            ((JSPrimitiveType) beanDecl.type).content
+                        );
+                    }
+                }
+            }
+        }
+        Assertions.assertTrue(hasMatchingBean);
+    }
+
+    interface Interf {
+
+        int get();
+
+        void set(int value);
+
+        String getStr();
+
+        void setStr(String value);
+    }
+
+    static abstract class AbsCls {
+
+        public abstract int get();
+
+        public abstract void set(int value);
+
+        public abstract String getStr();
+
+        public abstract void setStr(String value);
+    }
+}
