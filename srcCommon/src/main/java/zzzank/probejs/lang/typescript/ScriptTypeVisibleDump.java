b0a76f74acda5195c459910bb0f8b66ea2f29ef1
ZZZank <3410764033@qq.com>
2026-01-01 20:54:25 +0800

    forgot to commit `ScriptTypeVisibleDump`

---
+++ srcCommon/src/main/java/zzzank/probejs/lang/typescript/ScriptTypeVisibleDump.java
@@ -0,0 +1,11 @@
+package zzzank.probejs.lang.typescript;
+
+import zzzank.probejs.api.dump.TSDump;
+
+/**
+ * @author ZZZank
+ */
+public interface ScriptTypeVisibleDump extends TSDump {
+
+    String scriptTypeString();
+}
