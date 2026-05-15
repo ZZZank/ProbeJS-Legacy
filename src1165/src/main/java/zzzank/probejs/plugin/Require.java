14542f46e42cd6f329b3065ae8c9d0ea22a9a1ea
ZZZank <47418975+ZZZank@users.noreply.github.com>
2026-05-15 19:28:55 +0800

    rewrite `require` to support new module structure

--- src1165/src/main/java/zzzank/probejs/plugin/Require.java
+++ src1165/src/main/java/zzzank/probejs/plugin/Require.java
@@ -2,10 +2,8 @@ package zzzank.probejs.plugin;
 
 import dev.latvian.kubejs.script.ScriptManager;
 import dev.latvian.mods.rhino.*;
-import dev.latvian.mods.rhino.util.HideFromJS;
-import lombok.val;
+import org.jetbrains.annotations.Nullable;
 import zzzank.probejs.ProbeConfig;
-import zzzank.probejs.lang.java.clazz.ClassPath;
 import zzzank.probejs.lang.java.remap.RemapperBridge;
 
 public class Require extends BaseFunction {
@@ -15,6 +13,9 @@ public class Require extends BaseFunction {
         this.manager = manager;
     }
 
+    /// ```javascript
+    /// const { $ArrayList, $HashMap } = require("java/util");
+    /// ```
     @Override
     public Object call(Context cx, Scriptable scope, Scriptable self, Object[] args) {
         if (!ProbeConfig.isolatedScopes.get()) {
@@ -23,47 +24,40 @@ public class Require extends BaseFunction {
                 ProbeConfig.isolatedScopes.name()
             ));
         }
-        String name;
-        if (args.length == 0 || (name = args[0].toString()).startsWith("./")) {
-            return new RequireWrapper(null, Undefined.instance);
+
+        String prefix;
+        if (args.length == 0 || (prefix = args[0].toString()).startsWith(".")) {
+            return new RequireWrapper(manager, null);
         }
 
-        try {
-            var loaded = manager.loadJavaClass(scope, new String[]{name});
-            var path = ClassPath.ofJava(loaded.getClassObject());
-            return new RequireWrapper(path, loaded);
-        } catch (Exception ignored) {
-            manager.type.console.error(String.format("Class '%s' not found, returning undefined value", name));
-            return new RequireWrapper(null, Undefined.instance);
-        }
+        return new RequireWrapper(manager, prefix.replace('/', '.'));
     }
 
     public static class RequireWrapper extends ScriptableObject {
-        private final ClassPath path;
-        private final Object clazz;
+        private final ScriptManager manager;
+        @Nullable
+        private final String prefix;
 
-        public RequireWrapper(ClassPath path, Object clazz) {
-            assert clazz == Undefined.instance || clazz instanceof NativeJavaClass;
-            this.path = path;
-            this.clazz = clazz;
+        private RequireWrapper(ScriptManager manager, @Nullable String prefix) {
+            this.manager = manager;
+            this.prefix = prefix;
         }
 
         @Override
         public String getClassName() {
-            return path.getRemappedName();
+            return "ProbeJSRequire";
         }
 
         @Override
         public Object get(String name, Scriptable start) {
-            if (path == null || name.equals(path.getSimpleName())) {
-                return clazz;
+            if (prefix == null) {
+                return Undefined.instance;
+            }
+            if (name.startsWith("$")) {
+                var args = new String[]{RemapperBridge.unmapClass(prefix + '.' + name.substring(1))};
+                return manager.loadJavaClass(start, args);
             }
             return super.get(name, start);
-        }
-
-        @HideFromJS
-        public ClassPath getPath() {
-            return path;
         }
     }
 }
--- src1201/src/main/java/zzzank/probejs/plugin/Require.java
+++ src1201/src/main/java/zzzank/probejs/plugin/Require.java
@@ -2,9 +2,8 @@ package zzzank.probejs.plugin;
 
 import dev.latvian.mods.kubejs.script.ScriptManager;
 import dev.latvian.mods.rhino.*;
-import dev.latvian.mods.rhino.util.HideFromJS;
+import org.jetbrains.annotations.Nullable;
 import zzzank.probejs.ProbeConfig;
-import zzzank.probejs.lang.java.clazz.ClassPath;
 
 public class Require extends BaseFunction {
     private final ScriptManager manager;
@@ -13,55 +12,50 @@ public class Require extends BaseFunction {
         this.manager = manager;
     }
 
+    /// ```javascript
+    /// const { $ArrayList, $HashMap } = require("java/util");
+    /// ```
     @Override
     public Object call(Context cx, Scriptable scope, Scriptable self, Object[] args) {
         if (!ProbeConfig.isolatedScopes.get()) {
-            manager.scriptType.console.warn(String.format(
+            manager.scriptType.console.error(String.format(
                 "'require(...)' used without enabling '%s' config, this might cause inconsistency between IDE report and actual behaviour",
                 ProbeConfig.isolatedScopes.name()
             ));
         }
-        String name;
-        if (args.length == 0 || (name = args[0].toString()).startsWith("./")) {
-            return new RequireWrapper(null, Undefined.instance);
+
+        String prefix;
+        if (args.length == 0 || (prefix = args[0].toString()).startsWith(".")) {
+            return new RequireWrapper(manager, null);
         }
 
-        try {
-            var loaded = manager.loadJavaClass(name, true);
-            var path = ClassPath.ofJava(loaded.getClassObject());
-            return new RequireWrapper(path, loaded);
-        } catch (Exception ignored) {
-            manager.scriptType.console.error(String.format("Class '%s' not found, returning undefined value", name));
-            return new RequireWrapper(null, Undefined.instance);
-        }
+        return new RequireWrapper(manager, prefix.replace('/', '.'));
     }
 
     public static class RequireWrapper extends ScriptableObject {
-        private final ClassPath path;
-        private final Object clazz;
+        private final ScriptManager manager;
+        @Nullable
+        private final String prefix;
 
-        public RequireWrapper(ClassPath path, Object clazz) {
-            assert clazz == Undefined.instance || clazz instanceof NativeJavaClass;
-            this.path = path;
-            this.clazz = clazz;
+        private RequireWrapper(ScriptManager manager, @Nullable String prefix) {
+            this.manager = manager;
+            this.prefix = prefix;
         }
 
         @Override
         public String getClassName() {
-            return path.getRemappedName();
+            return "ProbeJSRequire";
         }
 
         @Override
         public Object get(Context cx, String name, Scriptable start) {
-            if (path == null || name.equals(path.getSimpleName())) {
-                return clazz;
+            if (prefix == null) {
+                return Undefined.instance;
+            }
+            if (name.startsWith("$")) {
+                return manager.loadJavaClass(prefix + '.' + name.substring(1), true);
             }
             return super.get(cx, name, start);
-        }
-
-        @HideFromJS
-        public ClassPath getPath() {
-            return path;
         }
     }
 }
